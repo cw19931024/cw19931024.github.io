a433256c4fb07c3bb02d9b3c1379c5dd83c74bb8
Chengwen <81548634@qq.com>
2020-05-29 17:38:08 +0800

    update

---
+++ build/precache-manifest.b4e690e37b0a15f7eb28a0fe678761ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9723a5986a5b0d46dd0f60b94da71ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d37eac98f9417431375",
+    "url": "/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "16756e778850f29c2c61",
+    "url": "/static/css/main.74d6f47d.chunk.css"
+  },
+  {
+    "revision": "7d37eac98f9417431375",
+    "url": "/static/js/2.7c9c562f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7c9c562f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16756e778850f29c2c61",
+    "url": "/static/js/main.f1c5ac03.chunk.js"
+  },
+  {
+    "revision": "c5c3ac691543ce7426d5",
+    "url": "/static/js/runtime-main.c3b3a7ec.js"
+  }
+]);
